1932858209075c41c3f692d1c770579aa54cfb92
Sarabjeet <sabysingh298@gmail.com>
2021-03-31 20:37:10 +0530

    First Hello world app

---
+++ my-firstapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = 'Brad'
+  return (
+    <div className="Container">
+      <h1>Hello from React by {name}</h1>
+    </div>
+  );
+}
+
+export default App;
